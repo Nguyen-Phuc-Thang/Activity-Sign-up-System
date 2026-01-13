76b722761d2e13c25697984aa4cdc47a3af254cb
Nguyen-Phuc-Thang <nguyen.phuc.thang.vn@gmail.com>
2026-01-13 20:40:00 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <main className='main-content'>
+        <Routes>
+
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
+}
