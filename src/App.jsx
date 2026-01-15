130e7fe3b05792ba08d7a0a0ed07b398a96e40db
Nguyen-Phuc-Thang <nguyen.phuc.thang.vn@gmail.com>
2026-01-15 12:48:59 +0700

    Login page and database initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
+// Screens imports
+import Login from './pages/Login.jsx';
+import Activity from './pages/Activity.jsx';
+
 function App() {
   return (
     <BrowserRouter>
       <main className='main-content'>
         <Routes>
-
+          <Route path="/" element={<Login />} />
+          <Route path="/activity" element={<Activity />} />
         </Routes>
       </main>
     </BrowserRouter>
   )
 }
+
+export default App;
---
+++ src/global/styles.js
@@ -0,0 +1,6 @@
+export const Colors = {
+    PRIMARY: '#ffffff',
+    SECONDARY: '#f76177',
+    BACKGROUND: '#f0f0f5',
+    TEXT: '#444040',
+}
